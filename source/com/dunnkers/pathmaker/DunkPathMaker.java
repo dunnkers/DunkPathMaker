3fbfc579d036ffe48c70ecc78728083ee44636e5
Dunnkers <dunnkers@hotmail.com>
2013-05-07 00:32:52 +0200

    Delete unnecessary code and refactor

--- source/com/dunnkers/pathmaker/DunkPathMaker.java
+++ source/com/dunnkers/pathmaker/DunkPathMaker.java
@@ -6,12 +6,10 @@ import com.dmurph.tracking.JGoogleAnalyticsTracker;
 import com.dmurph.tracking.JGoogleAnalyticsTracker.GoogleAnalyticsVersion;
 import com.dunnkers.pathmaker.ui.Window;
 
-
 /**
  * 
  * @author Dunnkers
  */
-// TODO create auto updater
 public class DunkPathMaker {
 
 	public static void main(String[] args) {
@@ -25,33 +23,16 @@ public class DunkPathMaker {
 		window.setVisible(true);
 	}
 
-	/*// adds an application track record to this analytic id
-	private static void track() {
-		final JGoogleAnalyticsTracker tracker = new JGoogleAnalyticsTracker(Configuration.APPLICATION_TITLE,
-				String.valueOf(Configuration.APPLICATION_VERSION),
-				"UA-40680192-1");
-		final FocusPoint focusPoint = new FocusPoint("Track");
-		tracker.trackAsynchronously(focusPoint);
-	}*/
-	
 	private static void track() {
 		JGoogleAnalyticsTracker.setProxy(System.getenv("http_proxy"));
-		AnalyticsConfigData config = new AnalyticsConfigData("UA-40680192-3");
-		// AWTSystemPopulator.populateConfigData(config); v 1.2.1 only
-		JGoogleAnalyticsTracker tracker = new JGoogleAnalyticsTracker(config,
+		final AnalyticsConfigData config = new AnalyticsConfigData("UA-40680192-3");
+		final JGoogleAnalyticsTracker tracker = new JGoogleAnalyticsTracker(config,
 				GoogleAnalyticsVersion.V_4_7_2);
-		//tracker.setDispatchMode(DispatchMode.SINGLE_THREAD);
-		// unnecessary, thread already created in main
-		
-		AnalyticsRequestData d = new AnalyticsRequestData();
-		d.setHostName("www.dunnkers.com");
-		d.setPageTitle("Application start");
-		d.setPageURL("com.dunnkers.pathmaker.DunkPathMaker.java");
-		d.setReferrer("www.dunnkers.com", "com.dunnkers");
-		tracker.makeCustomRequest(d);
-		/*d.setEventCategory("Application");
-		d.setEventAction("start");
-		tracker.makeCustomRequest(d);*/
-		//JGoogleAnalyticsTracker.completeBackgroundTasks(1000);
+		final AnalyticsRequestData request = new AnalyticsRequestData();
+		request.setHostName("www.dunnkers.com");
+		request.setPageTitle("Application start");
+		request.setPageURL("com.dunnkers.pathmaker.DunkPathMaker.java");
+		request.setReferrer("www.dunnkers.com", "com.dunnkers");
+		tracker.makeCustomRequest(request);
 	}
 }
