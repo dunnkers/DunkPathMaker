8d641b1c338721146fe79089ec16cff3060d63e9
Dunnkers <dunnkers@hotmail.com>
2013-05-07 00:31:19 +0200

    Change code accordingly

--- source/com/dunnkers/pathmaker/DunkPathMaker.java
+++ source/com/dunnkers/pathmaker/DunkPathMaker.java
@@ -1,7 +1,9 @@
 package com.dunnkers.pathmaker;
 
-import com.boxysystems.jgoogleanalytics.FocusPoint;
-import com.boxysystems.jgoogleanalytics.JGoogleAnalyticsTracker;
+import com.dmurph.tracking.AnalyticsConfigData;
+import com.dmurph.tracking.AnalyticsRequestData;
+import com.dmurph.tracking.JGoogleAnalyticsTracker;
+import com.dmurph.tracking.JGoogleAnalyticsTracker.GoogleAnalyticsVersion;
 import com.dunnkers.pathmaker.ui.Window;
 
 
@@ -23,12 +25,33 @@ public class DunkPathMaker {
 		window.setVisible(true);
 	}
 
-	// adds an application track record to this analytic id
+	/*// adds an application track record to this analytic id
 	private static void track() {
 		final JGoogleAnalyticsTracker tracker = new JGoogleAnalyticsTracker(Configuration.APPLICATION_TITLE,
 				String.valueOf(Configuration.APPLICATION_VERSION),
 				"UA-40680192-1");
 		final FocusPoint focusPoint = new FocusPoint("Track");
 		tracker.trackAsynchronously(focusPoint);
+	}*/
+	
+	private static void track() {
+		JGoogleAnalyticsTracker.setProxy(System.getenv("http_proxy"));
+		AnalyticsConfigData config = new AnalyticsConfigData("UA-40680192-3");
+		// AWTSystemPopulator.populateConfigData(config); v 1.2.1 only
+		JGoogleAnalyticsTracker tracker = new JGoogleAnalyticsTracker(config,
+				GoogleAnalyticsVersion.V_4_7_2);
+		//tracker.setDispatchMode(DispatchMode.SINGLE_THREAD);
+		// unnecessary, thread already created in main
+		
+		AnalyticsRequestData d = new AnalyticsRequestData();
+		d.setHostName("www.dunnkers.com");
+		d.setPageTitle("Application start");
+		d.setPageURL("com.dunnkers.pathmaker.DunkPathMaker.java");
+		d.setReferrer("www.dunnkers.com", "com.dunnkers");
+		tracker.makeCustomRequest(d);
+		/*d.setEventCategory("Application");
+		d.setEventAction("start");
+		tracker.makeCustomRequest(d);*/
+		//JGoogleAnalyticsTracker.completeBackgroundTasks(1000);
 	}
 }
