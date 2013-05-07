f4b21bca3206a8734532422f45ad463d9f15d8a8
Dunnkers <dunnkers@hotmail.com>
2013-05-08 00:05:46 +0200

    Simplify Resource getImage method

--- src/com/dunnkers/util/Resource.java
+++ src/com/dunnkers/util/Resource.java
@@ -3,6 +3,7 @@ package com.dunnkers.util;
 import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
@@ -29,17 +30,16 @@ public class Resource {
 
 	public Image getImage(final String path) {
 		try {
-			/* when loaded from .jar */
-			final Image image = ImageIO.read(getResourceClass().getClassLoader()
-					.getResource(path));
-			return image;
-		} catch (IllegalArgumentException e) {
+			final URL url = getResourceClass().getClassLoader()
+					.getResource(path);
 			/* when loaded from classes */
-			try {
+			if (url == null) {
 				return ImageIO.read(new File(path));
-			} catch (IOException e1) {
-				e1.printStackTrace();
 			}
+			/* when loaded from .jar */
+			return ImageIO.read(url);
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
