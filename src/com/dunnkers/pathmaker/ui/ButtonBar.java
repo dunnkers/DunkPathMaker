d912a6046e6231377e06828eaeb6a35a420ae55d
Dunnkers <dunnkers@hotmail.com>
2013-05-11 04:51:32 +0200

    Move ButtonBar into seperate file

---
+++ src/com/dunnkers/pathmaker/ui/ButtonBar.java
@@ -0,0 +1,40 @@
+package com.dunnkers.pathmaker.ui;
+
+import java.awt.Component;
+
+import javax.swing.JMenuBar;
+
+import com.dunnkers.pathmaker.Configuration;
+import com.dunnkers.pathmaker.ui.worldmap.WorldMapModel;
+
+public class ButtonBar extends JMenuBar {
+
+	private static final long serialVersionUID = 1L;
+	private final SettingsMenu settingsMenu;
+	private final MapMenu mapMenu;
+	private final HelpMenu help;
+
+	public ButtonBar(final WindowModel windowModel,
+			final WorldMapModel worldMapModel, final Component parentComponent) {
+		this.settingsMenu = new SettingsMenu("Settings",
+				windowModel,
+				worldMapModel,
+				parentComponent);
+		this.settingsMenu.setIcon(Configuration.ICON_SETTINGS.getIcon());
+		this.mapMenu = new MapMenu("Map");
+		this.mapMenu.setIcon(Configuration.ICON_MAP_16.getIcon());
+		this.help = new HelpMenu("Help", parentComponent);
+		this.help.setIcon(Configuration.ICON_HELP.getIcon());
+		init();
+	}
+
+	public void init() {
+		add(settingsMenu);
+		add(mapMenu);
+		add(help);
+	}
+
+	public SettingsMenu getSettingsMenu() {
+		return settingsMenu;
+	}
+}
