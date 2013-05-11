3b8978f28272cfa0f5cc832989b5db31126610f3
Dunnkers <dunnkers@hotmail.com>
2013-05-11 04:20:18 +0200

    Add a Frame class which uses the Window to create a JFrame

---
+++ src/com/dunnkers/pathmaker/ui/Frame.java
@@ -0,0 +1,42 @@
+package com.dunnkers.pathmaker.ui;
+
+import java.awt.Container;
+import java.awt.Image;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+import com.dunnkers.pathmaker.Configuration;
+import com.dunnkers.pathmaker.ui.Window.ButtonBar;
+import com.dunnkers.util.resource.ResourcePath;
+
+/**
+ * 
+ * @author Dunnkers
+ */
+public class Frame extends JFrame {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public Frame() {
+		final Window window = new Window();
+		final ButtonBar buttonBar = window.new ButtonBar();
+		this.setJMenuBar(buttonBar);
+		
+		final Container contentPane = this.getContentPane();
+		window.initContentPane(contentPane);
+		
+		this.setTitle(Configuration.WINDOW_TITLE);
+		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		final ArrayList<Image> icons = new ArrayList<Image>();
+		for (final ResourcePath ICON_MAP_X : Configuration.ICON_MAP) {
+			icons.add(ICON_MAP_X.getImage());
+		}
+		this.setIconImages(icons);
+		this.pack();
+		this.setSize(Configuration.WINDOW_SIZE);
+		this.setLocationRelativeTo(this.getOwner());
+	}
+}
