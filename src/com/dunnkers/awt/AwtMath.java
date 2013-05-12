35a1b1242c39082c175ac20561c380fad32c4d45
Dunnkers <dunnkers@hotmail.com>
2013-05-12 02:08:18 +0200

    Add a math function to the awt package

---
+++ src/com/dunnkers/awt/AwtMath.java
@@ -0,0 +1,21 @@
+package com.dunnkers.awt;
+
+import java.awt.Point;
+import java.awt.Rectangle;
+
+/**
+ * 
+ * @author Dunnkers
+ */
+public class AwtMath {
+	
+	public static Rectangle getRectangle(final Point pointOne, final Point pointTwo) {
+		final int diffX = pointOne.x - pointTwo.x;
+		final int diffY = pointOne.y - pointTwo.y;
+		final int x = diffX < 0 ? pointOne.x : pointTwo.x;
+		final int y = diffY < 0 ? pointOne.y : pointTwo.y;
+		final int width = Math.abs(diffX);
+		final int height = Math.abs(diffY);
+		return new Rectangle(x, y, width, height);
+	}
+}
